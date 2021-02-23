96f4f2ca56e2af29603bc294fb1f7194ec0d35ca
Thamizharasan <cdkthamizh27@gmail.com>
2021-02-23 19:55:17 +0530

    Initial commit

---
+++ src/Core/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
